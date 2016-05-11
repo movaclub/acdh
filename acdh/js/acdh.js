032f511e33112287eec59029347cf8814c03d837
movaclub <seeusers@gmail.com>
2016-05-12 00:43:06 +0300

    lists

--- acdh/js/acdh.js
+++ acdh/js/acdh.js
@@ -4,18 +4,17 @@
 	var app = angular.module('acdh', ['ui.router','ngSanitize']);
 	app.config(config);
 	var listURL = {
-		'knowmore':	'https://dhdev.eos.arz.oeaw.ac.at/en/api_0_1/nodes?parameters[type]=biblio',
-		'project':	'https://dhdev.eos.arz.oeaw.ac.at/en/api_0_1/nodes?parameters[type]=project',
-		'dha_page':	'https://dhdev.eos.arz.oeaw.ac.at/en/api_0_1/nodes?parameters[type]=dha_page',
-		'partners':	'https://dhdev.eos.arz.oeaw.ac.at/en/api_0_1/nodes?parameters[type]=institution'
+		'news':		'https://dhdev.eos.arz.oeaw.ac.at/en/api_0_1/nodes?parameters[type]=event&pagesize=all&callback=JSON_CALLBACK',		// news & events
+		'knowmore':	'https://dhdev.eos.arz.oeaw.ac.at/en/api_0_1/nodes?parameters[type]=biblio&pagesize=all&callback=JSON_CALLBACK',	// know more
+		'project':	'https://dhdev.eos.arz.oeaw.ac.at/en/api_0_1/nodes?parameters[type]=project&pagesize=all&callback=JSON_CALLBACK',	// projects
+		'partners':	'https://dhdev.eos.arz.oeaw.ac.at/en/api_0_1/nodes?parameters[type]=institution&pagesize=all&callback=JSON_CALLBACK'// partners (have in 'schema:keywords' the tag 'Partner')
 	};
+
 	app.controller('listCtrl',['$scope','$http', '$state', 'getLists', function($scope, $http, $state, getLists){
 	  $scope.Model = {};
 	  var getListPromise = getLists.getListPromise($state.current.name);
 	  getListPromise.then(
-		function(res){
-		  $scope.Model[$state.current.name] = res.data;  // console.log($state.current.name + ' $scope.Model: ', $scope.Model);
-		},
+		function(res){ $scope.Model[$state.current.name] = res.data;  console.log($state.current.name + ' $scope.Model: ', $scope.Model); },
 		function(err){ console.log('err: ', err); }
 	  );
 	}]);
@@ -24,6 +23,7 @@
 	  getList = {
 		getListPromise: function(state){console.log('STATE: ', state);
 		  var promise = $http.get(listURL[state]);
+// 		  var promise = $http.jsonp(listURL[state]);
 		  // deferObject =  deferObject || $q.defer(); // this one is needed when we need to cache the state data set (!), and not when we need to refresh data set for every state changed
 		  deferObject =  $q.defer();
 		  promise.then(
@@ -35,7 +35,17 @@
 	  };
 	  return getList;
 	}]);
-
+	app.filter('findPartner', function() { // for Partner's section (only)
+	  return function(items, field) {
+			var result = [];
+			angular.forEach(items, function(value) {
+				if (value.hasOwnProperty('schema:keywords') && value['schema:keywords'][0] == 'Partner') {
+					result.push(value);
+				}
+			});
+			return result;
+		};
+	});
 	function config($stateProvider, $urlRouterProvider, $locationProvider, $compileProvider){
 // 		$locationProvider.html5Mode(true).hashPrefix('!');
 		$compileProvider.debugInfoEnabled(true);
@@ -45,8 +55,8 @@
 	        url: '/news',
 	        views: {
 	            'content@': {
-	                templateUrl: '/acdh/js/views/news.html'//,
-					//controller: 'listInsCtrl'
+	                templateUrl: '/acdh/js/views/news.html',
+					controller: 'listCtrl'
 	            }
 	        }
 	    })
